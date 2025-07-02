aadc351535df1216cb76b318af667ac7b5f0f0ab
maxim-polyakov <maxim7012@gmail.com>
2025-07-03 01:02:04 +0300

    u

---
+++ server/api-gateway/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+
+app.use('/pets', createProxyMiddleware({ target: process.env.PET_SERVICE_URL, changeOrigin: true, }))
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`API Gateway running on port ${PORT}`)
+})
